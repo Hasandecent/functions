47223cd8f6d0bb20ea3eab84beeb26439c100c4a
Hasandecent <92886603+Hasandecent@users.noreply.github.com>
2021-11-18 08:48:15 +0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,46 @@
+// Question 1.Write a function that displays current date & time
+
+// function todayDate() {
+//     var todayDate = new Date();
+//     document.write(todayDate);
+// }
+// todayDate();
+
+// Question 2.Write a function that takes first & last name
+
+// function userName() {
+//     var firstName = prompt("Enter Your First Name");
+//     var lastName = prompt("Enter Your Last Name");
+//     alert("Have A Good Day" + " " + firstName + " " + lastName);
+// }
+// userName();
+
+// Question 3.Write a function that adds two numbers
+
+// var num1 = parseInt(prompt("Enter First Number"));
+// var num2 = parseInt(prompt("Enter Second Number"));
+// function add(num1, num2) {
+//     return alert(num1 + num2);
+// }
+// add(num1, num2)
+
+// Question 4.Write a function that takes three arguments
+
+// var num1 = +prompt("Enter Number 1")
+// var opt = prompt("Enter Operator + - x * / %")
+// var num2 = +prompt("Enter Number 2")
+// function calc(num1 ,opt , num2){    
+//     if(opt == "+"){        return num1 + num2 
+//        }   
+//      else if(opt == "-"){   
+//          return num1 - num2   
+//          }   
+//           else if (opt == "x"){   
+//              return num1 * num2    } 
+//         else if( opt == "/"){   
+//              return num1 / num2    }   
+//          else{   
+//           return "enter correct numer"    }
+// }
+// var add = calc(num1 ,opt , num2)
+// console.log(add)
